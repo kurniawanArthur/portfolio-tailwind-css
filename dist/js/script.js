4cb1a99de356f5a3ab696536556f99284fa5fa87
kurniawanArthur <krnwnarthur@gmail.com>
2023-01-14 17:25:34 +0700

    tambah arrow to top logo awan hilag ketika di scroll logo arrow to top hilang ketika di paling ujung atas

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,8 +5,18 @@ window.onscroll = function(){
 
     if(window.pageYOffset > fixNav){
         header.classList.add("navbar-fixed")
+        backToTop.classList.remove("hidden")
+        backToTop.classList.add("flex")
+        logo.removeChild(cloud)
+        headerAbout.classList.remove("lg:mr-16")
+        headerAbout.classList.remove("lg:ml-5")
     } else {
         header.classList.remove("navbar-fixed")
+        backToTop.classList.remove("flex")
+        backToTop.classList.add("hidden")
+        logo.insertBefore(cloud, awann)
+        headerAbout.classList.add("lg:mr-16")
+        headerAbout.classList.add("lg:ml-5")
     }
 }
 
@@ -19,3 +29,10 @@ hamburger.addEventListener("click", function(){
     navMenu.classList.toggle("hidden")
 })
 
+const backToTop = document.getElementById("backToTop")
+
+
+const logo = document.getElementById("logo")
+const cloud = document.getElementById("cloud")
+const awann = document.getElementById("awann")
+const headerAbout = document.getElementById("header-about")
