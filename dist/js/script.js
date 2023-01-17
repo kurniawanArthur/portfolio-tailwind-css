110dc6532485b4205f5bc9104e763b7caae01503
kurniawanArthur <krnwnarthur@gmail.com>
2023-01-18 01:16:47 +0700

    tambah waktu

--- dist/js/script.js
+++ dist/js/script.js
@@ -95,6 +95,29 @@ const html = document.querySelector("html")
 // Toggle cuaca sesuai local storage
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     logo.replaceChild(moon, cloud)
-  } else {
-    logo.replaceChild(cloud, moon)
+  } 
+
+
+// Tanggal dan Waktu
+function showTime() {
+    let currentTime = new Date();
+    let currentHours = currentTime.getHours();
+    let currentMinutes = currentTime.getMinutes();
+    let currentSeconds = currentTime.getSeconds();
+  
+    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+  
+    let timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+  
+    let currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+  
+    document.getElementById("time").innerHTML = currentTimeString;
+    setTimeout(showTime, 1000);
   }
+  
+  showTime();
+  
+  
